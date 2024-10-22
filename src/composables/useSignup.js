8625755deda6cc0d10d736b8c9de86c559b9e1e3
Anne Nguyen <baitoanngoisaodo@gmail.com>
2024-10-22 15:54:47 -0300

    completed Login and Signup Views

--- src/composables/useSignup.js
+++ src/composables/useSignup.js
@@ -1,29 +1,37 @@
-import { ref } from 'vue'
-import { projectAuth } from '../firebase/config'
+import { ref } from "vue";
+import { projectAuth } from "../firebase/config";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 
-const error = ref(null)
+const error = ref(null);
+const isPending = ref(false);
 
 const signup = async (email, password, displayName) => {
-  error.value = null
+  error.value = null;
+  isPending.value = true;
 
   try {
-    const res = await projectAuth.createUserWithEmailAndPassword(email, password)
+    const res = await createUserWithEmailAndPassword(
+      projectAuth,
+      email,
+      password
+    );
     if (!res) {
-      throw new Error('Could not complete signup')
+      throw new Error("Could not complete signup");
     }
-    await res.user.updateProfile({ displayName })
-    error.value = null
-    
-    return res
+    await updateProfile(res.user, { displayName });
+    error.value = null;
+    isPending.value = false;
+
+    return res;
+  } catch (err) {
+    console.log(err.message);
+    error.value = "The email address is already in use by another account.";
+    isPending.value = false;
   }
-  catch(err) {
-    console.log(err.message)
-    error.value = err.message
-  }
-}
+};
 
 const useSignup = () => {
-  return { error, signup }
-}
+  return { error, signup, isPending };
+};
 
-export default useSignup
+export default useSignup;
