6ffdad855aa9ac732a814278dee19d52ec47ef9f
Anne Nguyen <baitoanngoisaodo@gmail.com>
2024-10-23 22:15:18 -0300

    removed a comment

--- src/main.js
+++ src/main.js
@@ -8,7 +8,6 @@ import "./assets/main.css";
 
 let app;
 
-// to prevent when refreshing the page --> redirect to Login page if current user is already logged in
 projectAuth.onAuthStateChanged(() => {
   if (!app) {
     app = createApp(App).use(router).mount("#app");
