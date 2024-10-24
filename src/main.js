bcbe66a3e842b1697b6f8b596bfd42b51cddda41
Anne Nguyen <baitoanngoisaodo@gmail.com>
2024-10-23 22:14:34 -0300

    Fixed with projectAuth.onAuthStateChanged in main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { projectAuth } from "./firebase/config";
 
 // global styles
 import "./assets/main.css";
 
-createApp(App).use(router).mount("#app");
+let app;
+
+// to prevent when refreshing the page --> redirect to Login page if current user is already logged in
+projectAuth.onAuthStateChanged(() => {
+  if (!app) {
+    app = createApp(App).use(router).mount("#app");
+  }
+});
